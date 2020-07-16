8455a0de04d922fb0ba19e9f54e0edbd4610cd38
muzagov <muzagov95@mail.ru>
2020-07-16 22:02:55 +0300

    add task

---
+++ js7.js
@@ -0,0 +1,67 @@
+
+let newPlayer = function(name) {
+  create(name);
+  save();
+}
+
+function create(name) {
+  const level = 1;
+  
+  createUser(name, level);
+}
+
+function save() {
+  sendRequestToServer();
+}
+
+function sendRequestToServer() {}
+
+let generatePlayerName = function () {
+  return "player 1";
+}
+
+const name = generatePlayerName();
+
+newPlayer(name);
+create()
+  const level
+  createUser()
+save()
+  sendRequestToServer()
+
+
+
+
+
+
+
+
+
+
+function createNote() {
+  //какой-то код
+}
+
+function getMoney() {
+  return 1000;
+}
+
+(function() {
+  //some code
+})();
+
+const handleClick = function() {}
+
+let setBought = function() {
+  return [];
+}
+
+(function mySuperFunction() {
+  //super code
+})();
+
+let handleSubmit = function submit() {}
+
+const setCounter = function() {
+  return getMoney();
+}
